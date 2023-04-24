e3c9927748abaf05976da5e58bcb7fdf2ef4dbc9
Donald Miller <admin@evalgenie.com>
2023-04-24 22:17:24 +0000

    working test

---
+++ sg-test.py
@@ -0,0 +1,27 @@
+# using SendGrid's Python Library
+# https://github.com/sendgrid/sendgrid-python
+import os
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+import environ
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+# reading .env file
+environ.Env.read_env()
+
+message = Mail(
+    from_email='admin@evalgenie.com',
+    to_emails='admin@evalgenie.com',
+    subject='Sending with Twilio SendGrid is Fun',
+    html_content='<strong>and easy to do anywhere, even with Python</strong>')
+try:
+    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.message)
